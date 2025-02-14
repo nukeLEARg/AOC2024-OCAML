5afe557d76affc75c53bd16d748f489639888f5c
Nuke <nukelearyt@gmail.com>
2025-02-14 10:13:16 -0500

    more graph work

--- bin/Day16.ml
+++ bin/Day16.ml
@@ -1,6 +1,7 @@
 open Core
 open Core.Poly
 open Graph
+open Advent
 
 type direction =
   | East
@@ -8,21 +9,20 @@ type direction =
   | West
   | North
 
-module IntGraph = struct
+module CordDirMap = struct
   module Vertex = struct
     type t = int * int * direction
 
-    let compare (r1, c1, d1) (r2, c2, d2) =
-      match Stdlib.compare r1 r2 with
+    let compare (x1, y1, d1) (x2, y2, d2) =
+      match Stdlib.compare x1 x2 with
       | 0 ->
-        (match Stdlib.compare c1 c2 with
+        (match Stdlib.compare y1 y2 with
          | 0 -> Stdlib.compare d1 d2
          | c -> c)
       | c -> c
     ;;
 
     let hash (r, c, d) = Hashtbl.hash (r, c, d)
-
     let equal ((r1, c1, d1) : t) ((r2, c2, d2) : t) : bool = r1 = r2 && c1 = c2 && d1 = d2
   end
 
@@ -30,8 +30,6 @@ module IntGraph = struct
     type t = int
 
     let compare = Stdlib.compare
-    let hash = Hashtbl.hash
-    let equal = ( = )
     let default = 0
   end
 
@@ -39,17 +37,17 @@ module IntGraph = struct
 end
 
 module Display = struct
-  include IntGraph
+  include CordDirMap
 
-  let vertex_name ((r, c, d) : Vertex.t) : string =
+  let vertex_name ((x, y, d) : Vertex.t) : string =
     let dir_str =
       match d with
-      | East -> "East"
-      | South -> "South"
-      | West -> "West"
-      | North -> "North"
+      | East -> "E"
+      | South -> "S"
+      | West -> "W"
+      | North -> "N"
     in
-    Printf.sprintf "v_%d_%d_%s" r c dir_str
+    Printf.sprintf "\"%d,%d:%s\"" x y dir_str
   ;;
 
   let graph_attributes _ = []
@@ -62,13 +60,13 @@ end
 
 module Dot = Graphviz.Dot (Display)
 
-let graph = IntGraph.create ()
+let graph = CordDirMap.create ()
 
 let vertices =
   [ 1, 0, East; 2, 0, East; 3, 0, East; 4, 0, East; 5, 0, East; 6, 0, East; 7, 0, East ]
 ;;
 
-let () = List.iter ~f:(IntGraph.add_vertex graph) vertices
+let () = List.iter ~f:(CordDirMap.add_vertex graph) vertices
 
 let edges =
   [ (1, 0, East), (3, 0, East), 3
@@ -84,12 +82,54 @@ let edges =
   ]
 ;;
 
-let () = List.iter ~f:(fun (v1, v2, w) -> IntGraph.add_edge_e graph (v1, w, v2)) edges
+let () =
+  List.iter edges ~f:(fun (v1, v2, w) ->
+    CordDirMap.add_edge_e graph (v1, w, v2);
+    CordDirMap.add_edge_e graph (v2, w, v1))
+;;
 
 let () =
   Dot.output_graph stdout graph;
-  let dot_file = open_out "graph.dot" in
+  let dot_file = Out_channel.create "graph.dot" in
   Dot.output_graph dot_file graph;
-  close_out dot_file;
-  Printf.printf "file written to graph.dot\n"
+  Out_channel.close dot_file;
+  Printf.printf "File written to graph.dot\n"
+;;
+
+module W = struct
+  type edge = CordDirMap.E.t
+  type t = int
+
+  let weight x = CordDirMap.E.label x
+  let zero = 0
+  let add = ( + )
+  let sub = ( - )
+  let compare = compare
+end
+
+module DijkstraAlgo = Path.Dijkstra (CordDirMap) (W)
+
+let start_vertex = 1, 0, East
+let end_vertex = 2, 0, East
+
+let () =
+  try
+    let edges, total_weight = DijkstraAlgo.shortest_path graph start_vertex end_vertex in
+    Printf.printf
+      "Shortest path from %s to %s has weight %d:\n"
+      (Display.vertex_name start_vertex)
+      (Display.vertex_name end_vertex)
+      total_weight;
+    List.iter edges ~f:(fun (v1, w, v2) ->
+      Printf.printf
+        "%s --[%d]--> %s\n"
+        (Display.vertex_name v1)
+        w
+        (Display.vertex_name v2))
+  with
+  | Not_found ->
+    Printf.printf
+      "No path found from %s to %s\n"
+      (Display.vertex_name start_vertex)
+      (Display.vertex_name end_vertex)
 ;;
